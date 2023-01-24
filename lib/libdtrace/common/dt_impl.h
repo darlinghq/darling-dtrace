db956ae7d3c9ed6341629d948247c062e1690d18
Thomas A <curioustommy@protonmail.com>
2023-01-24 12:46:33 -0800

    Workaround yytext being a different type

--- lib/libdtrace/common/dt_impl.h
+++ lib/libdtrace/common/dt_impl.h
@@ -737,7 +737,11 @@ extern dt_pcb_t *yypcb;		/* pointer to current parser control block */
 extern char yyintprefix;	/* int token prefix for macros (+/-) */
 extern char yyintsuffix[4];	/* int token suffix ([uUlL]*) */
 extern int yyintdecimal;	/* int token is decimal (1) or octal/hex (0) */
+#ifdef DARLING
+extern char *yytext;
+#else
 extern char yytext[];		/* lex input buffer */
+#endif
 extern int yylineno;		/* lex line number */
 extern int yydebug;		/* lex debugging */
 extern dt_node_t *yypragma;	/* lex token list for control lines */
